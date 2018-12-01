ed6b302efb21297130d5a12cc79bc99fe3b92232
Maryam <mkaka139@gmail.com>
2018-12-01 15:27:37 -0500

    Set up message component to display information from message service

--- angular-tour-of-heros/src/app/hero.service.ts
+++ angular-tour-of-heros/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROS } from './mock-heros';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,9 +10,12 @@ import { Observable, of } from 'rxjs';
 
 export class HeroService {
 
-	constructor() { }
+	constructor(
+		private messageService: MessageService
+	) { }
 
 	getHeros(): Observable<Hero[]> {
+		this.messageService.add("HeroService: fetched hero");
 		return of(HEROS);
 	}
 }
